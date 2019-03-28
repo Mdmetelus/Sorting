de6f72cfa7ab6b0dea1b5dedd7a810ba3ffa9e94
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-28 19:03:52 -0400

    mvp complete

--- src/recursive_sorting.py
+++ src/recursive_sorting.py
@@ -5,8 +5,19 @@ def merge( arrA, arrB ):
     # TO-DO
     a = 0
     b = 0
-
-    
+    for i in range(0, elements):
+        if a >= len(arrA): 
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB): 
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:  
+            merged_arr[i] = arrA[a]
+            a += 1
+        else: 
+            merged_arr[i] = arrB[b]
+            b += 1
     return merged_arr
 
 
